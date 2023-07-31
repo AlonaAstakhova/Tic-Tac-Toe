a60eb8030d5a81ec1514d53f7de25565e706032b
AlonaAstakhova <alyonasta@gmail.com>
2023-07-30 19:09:42 -0700

    branch issue

---
+++ src/App.js
@@ -0,0 +1,80 @@
+import { useState, useEffect } from 'react'
+import { Cell } from './components/Cell.js'
+import { ResetButton } from './components/ResetButton.js'
+
+function App() {
+  const [cells, setCells] = useState(Array(9).fill(null))
+  const [go, setGo] = useState('cross')
+  const [winMessage, setWinMessage] = useState(null)
+
+  const message = 'Now ' + go + ' is going!'
+
+  let messageColor = go === 'circle' ? 'p blue' : 'p red'
+
+  const checkWinner = () => {
+    const winCombos = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ]
+
+    winCombos.forEach((arr) => {
+      let circleWins = arr.every((cell) => cells[cell] === 'circle')
+
+      if (circleWins) {
+        setGo('circle')
+        setWinMessage('Circle WINS!')
+        return
+      }
+    })
+
+    winCombos.forEach((arr) => {
+      let crossWins = arr.every((cell) => cells[cell] === 'cross')
+
+      if (crossWins) {
+        setGo('cross')
+        setWinMessage('Cross WINS!')
+        return
+      }
+    })
+  }
+
+  useEffect(() => {
+    checkWinner()
+  }, [cells])
+
+  const resetBoard = () => {
+    setCells(Array(9).fill(null))
+    setGo('cross')
+    setWinMessage(null)
+  }
+
+  return (
+    <div className="app">
+      <h1>Tic Tac Toe</h1>
+      <p className={messageColor}> {winMessage || message}</p>
+      <div className="board">
+        {cells.map((cell, id) => (
+          <Cell
+            key={id}
+            id={id}
+            cell={cell}
+            setCells={setCells}
+            go={go}
+            setGo={setGo}
+            cells={cells}
+            winMessage={winMessage}
+          />
+        ))}
+        <ResetButton resetBoard={resetBoard} />
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Cell.js
@@ -0,0 +1,42 @@
+export function Cell({ id, cell, setCells, go, setGo, cells, winMessage}) {
+  const handleClick = (el) => {
+    const value =
+      el.target.firstChild.classList.contains('circle') ||
+      el.target.firstChild.classList.contains('cross')
+
+    if (!value) {
+      if (go === 'circle') {
+        el.target.firstChild.classList.add('circle')
+        handleCellChange('circle')
+        setGo('cross')
+      }
+      if (go === 'cross') {
+        el.target.firstChild.classList.add('cross')
+        handleCellChange('cross')
+        setGo('circle')
+      }
+    } else {
+      value = 'circle'
+    }
+  }
+
+  const handleCellChange = (className) => {
+    const newCells = cells.map((cell, index) => {
+      if (index === id) {
+        return className
+      } else {
+        return cell
+      }
+    })
+    setCells(newCells)
+  }
+
+  return (
+    <div
+      className="box"
+      id={id}
+      onClick={!winMessage && cell === null ? handleClick : undefined}>
+      <div className={cell}></div>
+    </div>
+  )
+}
